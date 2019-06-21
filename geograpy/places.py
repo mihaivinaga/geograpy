bf787b90b6a13546e25809335e137013f5d9863a
Mihai Vinaga <vinaga.mihai@gmail.com>
2019-06-21 10:00:37 +0300

    like query

--- geograpy/places.py
+++ geograpy/places.py
@@ -57,6 +57,8 @@ class PlaceContext(object):
         ]
         geos = {
             "country": ['country_iso_code_key="%s"', 'secondary_iso_code_key="%s"', 'country_name_key="%s"'],
+            "country_region": ['subdivision_1_iso_code_key="%s"', 'subdivision_1_name_key="%s"',
+                               'subdivision_2_iso_code like "%s"', 'subdivision_2_name  like "%s"'],
             "state": ['subdivision_1_iso_code_key="%s"', 'subdivision_1_name_key="%s"',
                       'subdivision_2_iso_code like "%s"', 'subdivision_2_name  like "%s"'],
             "city": ['city_name_key like "%s%%"', 'city_name_key like "%%%s"', 'city_name_v2_key like "%%%s"',
